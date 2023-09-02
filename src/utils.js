2fc7321cf6026f58dd52a46ac8b750e12b665e1d
Leonardo Tricotti <tricottileo@gmail.com>
2023-09-02 19:11:54 -0300

    commit

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,35 @@
-import { dirname } from "path";
 import bcrypt from "bcrypt";
+import { dirname } from "path";
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
 import { fileURLToPath } from "url";
+
+dotenv.config();
 
 //Rutas de archivos
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+// Clave secreta
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+// Token de autenticación
+export const generateToken = (user) => {
+  const token = jwt.sign({ user }, PRIVATE_KEY, { expiresIn: "1h" });
+  return token;
+};
+
+export const authToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader)
+    return res.status(401).json({ message: "No estas autorizado" });
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, PRIVATE_KEY, (error, credentials) => {
+    if (error) return res.status(403).json({ message: "Token no valido" });
+    req.user = credentials.user;
+    next();
+  });
+};
 
 //Encriptar contraseña
 export const createHash = (password) =>
